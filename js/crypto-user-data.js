93868bf89a1a25bf90021d6cb3b6132e0d735a19
cafrex <cafrex@users.noreply.github.com>
2017-10-28 09:24:54 +0200

    movimientos

--- js/crypto-user-data.js
+++ js/crypto-user-data.js
@@ -3,7 +3,7 @@
  */
 var userCoinBalanceStatic = {
 		'david': {
-			'EUR': 13077.35,
+			'EUR': 10956.35,
 			'BTC': 0.00009816,
 			'ETH': 31.77052414,
 			'LTC': 0.00004317,
@@ -47,7 +47,8 @@ var userAccountMovementsStatic = {
 			                          {'date': '05/04/2017', 'amount': 1300},
 			                          {'date': '07/04/2017', 'amount': 1000.36},
 			                          {'date': '09/07/2017', 'amount': 408},
-			                          {'date': '13/10/2017', 'amount': 2000}
+			                          {'date': '13/10/2017', 'amount': 2000},
+			                          {'date': '28/10/2017', 'amount': 2121}
 			                          ]
 							},
 							'carmen': {
